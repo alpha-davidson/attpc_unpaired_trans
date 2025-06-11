b2f40816eef6e879f8ede83e2b70f4a5df029b4c
allandolfi <allandolfi@davidson.edu@alpha.ada.davidson.edu>
2025-06-11 13:20:22 -0400

    removed gpu request

--- jupyter.sh
+++ jupyter.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name "Jupyter Instance"
 #SBATCH --output jupyter.log
 #SBATCH --mem 32g
-#SBATCH --gpus 1
 
 source /opt/conda/bin/activate dpm-pc-gen
 jupyter lab --port=1235 --ip=0.0.0.0
