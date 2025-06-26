d4594198f9e2b4986c21ed45191a59e605aa76e6
allandolfi <allandolfi@davidson.edu@alpha.ada.davidson.edu>
2025-06-26 16:42:44 -0400

    for checkout

--- train/trainDPM.sh
+++ train/trainDPM.sh
@@ -8,5 +8,5 @@
 
 cd /home/DAVIDSON/allandolfi/attpc_unpaired_trans
 # train generator
-python -m train.train_gen --dataset_path data/Fission/fission_data/Fission_exp.npy --max_iters 1510000 --val_freq 100 --tag FissionExp --lr 1e-3 --end_lr 1e-7
+python -m train.train_gen --dataset_path data/Fission/fission_data/Fission_sim.npy --max_iters 3000000 --val_freq 100 --tag FissionSim --lr 1e-3 --end_lr 1e-7
 
