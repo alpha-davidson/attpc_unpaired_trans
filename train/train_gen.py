4921e453b56b9acebd467ea2a6f7fcab30e78510
allandolfi <allandolfi@davidson.edu@alpha.ada.davidson.edu>
2025-07-09 09:21:18 -0400

    final fixes

--- train/train_gen.py
+++ train/train_gen.py
@@ -306,4 +306,4 @@ with open(lr_val_log_path, mode) as f:
     f.write("\n")
 
     for epoch_num, val_loss in zip(val_epochs, epoch_val_losses):
-    f.write(f"Epoch {epoch_num}: Val Loss {val_loss:.6f}\n"
+        f.write(f"Epoch {epoch_num}: Val Loss {val_loss:.6f}\n")
